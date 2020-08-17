aeb242db82485850a89967c6bdd2276214eeaa23
DerTolleIgel <demoekelkrit@gmx.de>
2020-08-17 20:58:29 +0200

    Camera Controller
    fsdafs

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = player.transform.position;
+        v.z = -10;
+        GetComponent<Transform>().SetPositionAndRotation(v, new Quaternion());
+    }
+}
