ba4327e96623eec525bfa22194a1d71b4983bfd0
Daniel <letmesaphir@web.de>
2020-08-18 18:01:18 +0200

    added a miserable test for collider + transition, which Mario has to fix

---
+++ Assets/ChangeScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ChangeScreen : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject nextScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+/*
+ * taken from here: https://answers.unity.com/questions/1368545/how-do-i-create-a-scene-variable.html
+ public string levelName;
+  
+      void OnTriggerEnter2D(Collider2D coll) {
+  
+          if(coll.gameObject.name == "Player") {
+              // Loading the scene from it's name
+              SceneManager.LoadScene(levelName);
+          }
+      }
+*/
