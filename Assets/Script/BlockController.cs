8c1d2e75c1b13585632ab8a8a31bac63a57754e2
DerTolleIgel <demoekelkrit@gmx.de>
2020-08-18 21:53:36 +0200

    Better Jump + Block
    You ever felt like you want to jump like.. a video game character? well now these video game characters can.. ugh

---
+++ Assets/Script/BlockController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    private bool active = true;
+
+    public Sprite spriteActive;
+    public Sprite spriteInactive; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // test code
+        //if (Time.time % 5 == 0) changeStatus();
+    }
+
+    public void changeStatus()
+    {
+        active = !active;
+
+        if(active)
+        {
+            GetComponent<SpriteRenderer>().sprite = spriteActive;
+            gameObject.layer = LayerMask.NameToLayer("Walkable");
+        } else
+        {
+            GetComponent<SpriteRenderer>().sprite = spriteInactive;
+            gameObject.layer = LayerMask.NameToLayer("Empty");
+        }
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -13,20 +13,21 @@ public class PlayerMovement : MonoBehaviour
 
 
     public float speed = 10;
-    public float jumpPower = 3;
-    public float jumpTicks = 10;
-    private bool inJump1 = false;
-    private float inJumpTime1 = 0;
-
-    private bool inJump2 = false;
-    private float inJumpTime2 = 0;
-
-
-
+    public float jumpPower = 3;    
     private Vector2 move1;
     private Vector2 move2;
+
     private bool jump1;
     private bool jump2;
+    private bool jumpdown1;
+    private bool jumpdown2;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    private Rigidbody2D rb1;
+    private Rigidbody2D rb2;
+
 
 
 
@@ -37,12 +38,18 @@ public class PlayerMovement : MonoBehaviour
         controls.GamePlay.Player1Move.performed += ctx => move1 = ctx.ReadValue<Vector2>();
         controls.GamePlay.Player2Move.performed += ctx => move2 = ctx.ReadValue<Vector2>();
         controls.GamePlay.Player1Move.canceled += ctx => move1 = Vector2.zero;
-        controls.GamePlay.Player2Move.canceled += ctx => move2 = Vector2.zero;
+        controls.GamePlay.Player2Move.canceled += ctx => move2 = Vector2.zero; 
 
-        controls.GamePlay.Player1Jump.performed += ctx => jump1 = true; //onPlayer1Jump();
-        controls.GamePlay.Player2Jump.performed += ctx => jump2 = true;//onPlayer2Jump();
-        controls.GamePlay.Player1Jump.canceled += ctx => jump1 = false;
-        controls.GamePlay.Player2Jump.canceled += ctx => jump2 = false;
+        controls.GamePlay.Player1Jump.started += ctx => jump1 = true;
+        controls.GamePlay.Player2Jump.started += ctx => jump2 = true;
+        controls.GamePlay.Player1Jump.performed += ctx => jumpdown1 = true;
+        controls.GamePlay.Player2Jump.performed += ctx => jumpdown2 = true;
+        controls.GamePlay.Player1Jump.canceled += ctx => jumpdown1 = false;
+        controls.GamePlay.Player2Jump.canceled += ctx => jumpdown2 = false;
+
+        rb1 = player1.GetComponent<Rigidbody2D>();
+        rb2 = player2.GetComponent<Rigidbody2D>();
+
     }
 
     private void OnEnable()
@@ -55,71 +62,78 @@ public class PlayerMovement : MonoBehaviour
         controls.GamePlay.Disable();
     }
 
+    private void Update()
+    {
+        //bool mayJump = false;
+        //if (jump1)
+        //{
+        //    mayJump = player1.GetComponent<BoxCollider2D>().IsTouchingLayers(-1);
+        //    if (mayJump)
+        //    player1.GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpPower;
+        //    jump1 = false;
+        //}
+        //if (jump2)
+        //{
+        //    mayJump = player2.GetComponent<BoxCollider2D>().IsTouchingLayers(-1);
+        //    if (mayJump)
+        //    player2.GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpPower;
+        //    jump2 = false;
+        //}
+    }
+
     void FixedUpdate()
     {
-                
-        Rigidbody2D rb = player1.GetComponent<Rigidbody2D>();
-        float h = move1.x;
+        bool mayJump;
+        float h;
+        float v;
+        //player 1
+        h = move1.x;
         if (h > 0) player1.transform.eulerAngles = new Vector3(0, 0, 0);
         if (h < 0) player1.transform.eulerAngles = new Vector3(0, 180, 0);
-        float v = rb.velocity.y;
-
-        bool mayJump = player1.GetComponent<BoxCollider2D>().IsTouchingLayers(-1);
-        
-        if (jump1 && (mayJump || inJump1 ) )
+        v = rb1.velocity.y;
+        if (jump1)
         {
+            mayJump = player1.GetComponent<BoxCollider2D>().IsTouchingLayers(-1);
+            if (mayJump)
+                v = jumpPower;
+            jump1 = false;
+        }
+        
 
-            if (inJumpTime1 == 0)
-                v += jumpPower;
-            else v += jumpPower / 2;
+        if (v < 0)
+        {
+            v += Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } else if (!jumpdown1)
+        {
+            v += Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        Vector2 dir = new Vector2(h * speed, v);
+        rb1.velocity = dir;
 
-            inJumpTime1 = inJumpTime1 + 1;
-            //Debug.Log("JumpTime " + inJumpTime + " // " + Time.time);
-            if (inJumpTime1 >= jumpTicks) {
-                inJump1 = false; 
-                inJumpTime1 = 0;
-            }
-            else
-            { 
-                inJump1 = true;
-            }
-        };
-
-        Vector2 dir = new Vector2(h* speed, v);
-        player1.GetComponent<Rigidbody2D>().velocity = dir;
-
-        // player 2
-
-        rb = player2.GetComponent<Rigidbody2D>();
+        // player 2        
         h = move2.x;
         if (h > 0) player2.transform.eulerAngles = new Vector3(0, 0, 0);
         if (h < 0) player2.transform.eulerAngles = new Vector3(0, 180, 0);
-        v = rb.velocity.y;
-
-        mayJump = player2.GetComponent<BoxCollider2D>().IsTouchingLayers(-1);
-
-        if (jump2 && (mayJump || inJump2))
+        v = rb2.velocity.y;
+        if (jump2)
         {
+            mayJump = player2.GetComponent<BoxCollider2D>().IsTouchingLayers(-1);
+            if (mayJump)
+                v = jumpPower;
+            jump2 = false;
+        }
 
-            if (inJumpTime2 == 0)
-                v += jumpPower;
-            else v += jumpPower / 2;
-
-            inJumpTime2 = inJumpTime2 + 1;
-            //Debug.Log("JumpTime " + inJumpTime + " // " + Time.time);
-            if (inJumpTime2 >= jumpTicks)
-            {
-                inJump2 = false;
-                inJumpTime2 = 0;
-            }
-            else
-            {
-                inJump2 = true;
-            }
-        };
+        if (v < 0)
+        {
+            v += Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (!jumpdown2)
+        {
+            v += Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
 
         dir = new Vector2(h * speed, v);
-        player2.GetComponent<Rigidbody2D>().velocity = dir;
+        rb2.velocity = dir;
 
     }
 
