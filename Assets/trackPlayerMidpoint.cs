d387438347de60fff37afc60608e73e1694f3d9a
DerTolleIgel <demoekelkrit@gmx.de>
2020-08-19 23:40:41 +0200

    Cinemachine

---
+++ Assets/trackPlayerMidpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackPlayerMidpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player1;
+    public GameObject player2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = (player1.transform.position + player2.transform.position) / 2;
+    }
+}
