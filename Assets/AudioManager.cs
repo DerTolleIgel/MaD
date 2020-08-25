cfa4f836cb4e2245e414e230171716a685898a1e
DerTolleIgel <demoekelkrit@gmx.de>
2020-08-25 23:29:38 +0200

    Audio Manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    static private AudioManager instance;
+
+    public Sound[] sounds;
+    
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.loop = sound.loop;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+        }
+    }
+
+    private void Start()
+    {
+        Play("songSilence");
+        
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound not found");
+            return;
+        }
+        s.source.Play();
+    }
+
+
+    static AudioManager get()
+    {
+        return instance;
+    }
+}
